76861df74bc828794ca012cccdae84aaee86def1
Fishbowler <dan@caseley.me.uk>
2017-05-23 16:07:42 +0100

    Move app from defaulting to common port

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.delete('/emails/:msgId', function(req, res){
 	
 });
 
-var server = app.listen(8080, function (){
+var server = app.listen(8081, function (){
 	var host = server.address().address;
 	var port = server.address().port;
 	
